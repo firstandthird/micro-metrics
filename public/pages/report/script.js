e3e28fe85495e07c61f223c7f4f7b88fc2a12d42
Greg Allen <greg@firstandthird.com>
2016-09-18 21:04:12 -0700

    initial work on report ui

---
+++ public/pages/report/script.js
@@ -0,0 +1,17 @@
+/* global window */
+const MG = require('metrics-graphics');
+
+const data = window._data.map((item) => ({ // eslint-disable-line no-underscore-dangle
+  date: new Date(item.createdOn),
+  value: item.value
+}));
+
+MG.data_graphic({
+  title: 'Report',
+  data,
+  full_width: true,
+  height: 400,
+  target: '#report', // the html element that the graphic is inserted in
+  x_accessor: 'date',  // the key that accesses the x value
+  y_accessor: 'value' // the key that accesses the y value
+});
---
+++ routes/ui/report.js
@@ -0,0 +1,21 @@
+exports.report = {
+  path: '/ui',
+  method: 'get',
+  config: {
+    pre: [
+      [
+        { assign: 'types', method: 'types()' },
+        { assign: 'tags', method: 'tags(query)' },
+        { assign: 'report', method: 'get(query)' }
+      ]
+    ]
+  },
+  handler(request, reply) {
+    reply.view('report/view', {
+      selectedType: request.query.type,
+      types: request.pre.types,
+      tags: request.pre.tags,
+      report: request.pre.report
+    });
+  }
+};
