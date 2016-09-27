c1669769e3948471f0b06cbcfb25e4f0ad6e9525
Chris <orthagonal1@gmail.com>
2016-09-27 12:05:12 -0500

    /api/report now supports minutes

--- test/test.report.js
+++ test/test.report.js
@@ -3,6 +3,7 @@ const code = require('code');
 const lab = exports.lab = require('lab').script();
 const setup = require('./setup.test.js');
 
+const twentyMinutes = 1000 * 60 * 20;
 const twoDays = 1000 * 60 * 60 * 24 * 2;
 const threeHours = 1000 * 60 * 60 * 3;
 const current = new Date().getTime();
@@ -30,6 +31,14 @@ lab.experiment('type', { timeout: 5000 }, () => {
       value: 142000000,
       data: 'purchasing account',
       userId: 'Montgomery Burns',
+    },
+    {
+      type: 'BankAccount',
+      tags: { currency: 'dollars', units: 'cents' },
+      value: 0.15,
+      data: 'liquid assets only',
+      userId: '20m',
+      createdOn: new Date(current - twentyMinutes)
     },
     {
       type: 'BankAccount',
@@ -73,7 +82,7 @@ lab.experiment('type', { timeout: 5000 }, () => {
       url: '/api/report'
     }, (response) => {
       code.expect(response.statusCode).to.equal(200);
-      code.expect(response.result.count).to.equal(7);
+      code.expect(response.result.count).to.equal(8);
       done();
     });
   });
@@ -84,7 +93,7 @@ lab.experiment('type', { timeout: 5000 }, () => {
       url: '/api/report?last=3h'
     }, (response) => {
       code.expect(response.statusCode).to.equal(200);
-      code.expect(response.result.count).to.equal(1);
+      code.expect(response.result.count).to.equal(2);
       done();
     });
   });
@@ -95,7 +104,7 @@ lab.experiment('type', { timeout: 5000 }, () => {
       url: '/api/report?last=2d'
     }, (response) => {
       code.expect(response.statusCode).to.equal(200);
-      code.expect(response.result.count).to.equal(2);
+      code.expect(response.result.count).to.equal(3);
       done();
     });
   });
@@ -106,7 +115,7 @@ lab.experiment('type', { timeout: 5000 }, () => {
       url: '/api/report?last=2d1h'
     }, (response) => {
       code.expect(response.statusCode).to.equal(200);
-      code.expect(response.result.count).to.equal(3);
+      code.expect(response.result.count).to.equal(4);
       done();
     });
   });
@@ -115,6 +124,17 @@ lab.experiment('type', { timeout: 5000 }, () => {
     setup.server.inject({
       method: 'GET',
       url: '/api/report?last=4h1d'
+    }, (response) => {
+      code.expect(response.statusCode).to.equal(200);
+      code.expect(response.result.count).to.equal(3);
+      done();
+    });
+  });
+  lab.test('can use the report method to get a list of metrics from the db by hour and day and minute', { timeout: 5000 }, (done) => {
+    code.expect(true).to.equal(true);
+    setup.server.inject({
+      method: 'GET',
+      url: '/api/report?last=25m'
     }, (response) => {
       code.expect(response.statusCode).to.equal(200);
       code.expect(response.result.count).to.equal(2);
@@ -128,7 +148,7 @@ lab.experiment('type', { timeout: 5000 }, () => {
       url: '/api/report?type=BankAccount'
     }, (response) => {
       code.expect(response.statusCode).to.equal(200);
-      code.expect(response.result.count).to.equal(4);
+      code.expect(response.result.count).to.equal(5);
       done();
     });
   });
