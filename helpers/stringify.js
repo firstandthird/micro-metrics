6f5c88129fcf83e0d95cbe6732c261012108d8b3
Greg Allen <greg@firstandthird.com>
2016-09-26 23:44:47 -0700

    stringify helper - if no input, just return blank string

--- helpers/stringify.js
+++ helpers/stringify.js
@@ -1,5 +1,8 @@
 const Handlebars = require('handlebars');
 module.exports = function(input, pretty) {
+  if (typeof input === 'undefined') {
+    return '';
+  }
   const spaces = (pretty) ? 2 : 0;
   return new Handlebars.SafeString(JSON.stringify(input, null, spaces));
 };
