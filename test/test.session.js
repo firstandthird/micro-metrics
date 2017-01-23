fa40d57697d78be97d1b9199ce87b5098911559b
Eric Willis <eric@ecwillis.com>
2017-01-23 17:29:18 +0000

    Testing completed

--- test/test.session.js
+++ test/test.session.js
@@ -10,5 +10,21 @@ lab.experiment('sessions', { timeout: 5000 }, () => {
   lab.afterEach({ timeout: 5000 }, (done) => {
     setup.stop(done);
   });
-  lab.test('it can add a session and user id to the tracking payload');
+  lab.test('it can add a session and user id to the tracking payload', (done) => {
+    setup.server.inject({
+      method: 'POST',
+      url: '/api/track',
+      payload: {
+       type: 'page-view',
+       tags: 'tag1,tag2',
+       value: 1,
+       userId: '777',
+       sessionId: '9090'
+      }
+    }, (response) => {
+      code.expect(response.statusCode).to.equal(200);
+      code.expect(response.result.sessionId).to.equal('9090');
+      done();
+    });
+  });
 });
