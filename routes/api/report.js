205004d3026390d28b182da9493e8aedfcd583e1
Greg Allen <greg@firstandthird.com>
2017-04-19 14:54:43 -0700

    move report logic into report route

--- routes/api/report.js
+++ routes/api/report.js
@@ -3,16 +3,59 @@
 exports.report = {
   method: 'GET',
   path: '/api/report',
-  handler(request, reply) {
-    const query = request.query;
-    request.server.methods.get(query, (err, results) => {
-      if (err) {
-        return reply(err);
+  handler: {
+    autoInject: {
+      query(server, request, done) {
+        const filter = request.query || {};
+
+        const findObj = {};
+
+        if (filter.last) {
+          filter.startDate = server.methods.extractStartDate(filter.last);
+          filter.endDate = new Date().getTime();
+        }
+
+        if (filter.type) {
+          findObj.type = filter.type;
+        }
+
+        if (filter.tags) {
+          const tagObj = server.methods.stringToKeyValue(filter.tags);
+          Object.keys(tagObj).forEach((tag) => {
+            const value = tagObj[tag];
+            findObj[`tags.${tag}`] = (value === true) ? { $exists: 1 } : value;
+          });
+        }
+
+        if (filter.fields) {
+          const fields = server.methods.stringToKeyValue(filter.fields);
+          Object.keys(fields).forEach((field) => {
+            const value = fields[field];
+            findObj[`fields.${field}`] = (value === true) ? { $exists: 1 } : value;
+          });
+        }
+
+        if (filter.startDate && filter.endDate) {
+          findObj.createdOn = {
+            $gte: new Date(filter.startDate),
+            $lte: new Date(filter.endDate)
+          };
+        }
+
+        if (filter.value) {
+          findObj.value = (isNaN(filter.value / 1)) ? filter.value : filter.value / 1;
+        }
+        done(null, findObj);
+      },
+      find(query, server, done) {
+        server.db.tracks.find(query).sort({ createdOn: 1 }).toArray((err, results) => done(err, results));
+      },
+      reply(find, done) {
+        done(null, {
+          count: find.length,
+          results: find
+        });
       }
-      reply({
-        count: results.length,
-        results
-      });
-    });
+    }
   }
 };
