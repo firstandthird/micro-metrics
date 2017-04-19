53209342c02d3d75408a288a7c4480f884674171
Chris <orthagonal1@gmail.com>
2017-04-18 19:24:04 -0500

    add redirect test in tap

--- test/test.redirect.js
+++ test/test.redirect.js
@@ -1,36 +1,33 @@
 'use strict';
-const code = require('code');
-const lab = exports.lab = require('lab').script();
+const tap = require('tap');
 const setup = require('./setup.test.js');
 
-lab.experiment('tags', { timeout: 5000 }, () => {
-  lab.beforeEach({ timeout: 5000 }, (done) => {
-    setup.withRapptor({}, [], done);
-  });
-  lab.afterEach({ timeout: 5000 }, (done) => {
-    setup.stop(done);
-  });
+tap.beforeEach((done) => {
+  setup.withRapptor({}, [], done);
+});
+tap.afterEach((done) => {
+  setup.stop(done);
+});
 
-  lab.test('can use /r route to do a tracked redirect', (done) => {
-    setup.server.route({
-      path: '/detour',
-      method: 'GET',
-      handler: (req, res) => {
-        res.reply({ success: true });
-      }
-    });
-    setup.server.inject({
-      url: '/r/?to=/detour&type=thatType',
-      method: 'GET',
-    }, (response) => {
-      code.expect(response.statusCode).to.equal(302);
-      setup.server.db.tracks.findOne({ type: 'thatType' }, (err, track) => {
-        code.expect(err).to.equal(null);
-        code.expect(track.type).to.equal('thatType');
-        code.expect(track.data.ip).to.equal('127.0.0.1');
-        code.expect(track.data.userAgent).to.equal('shot');
-        done();
-      });
+tap.test('can use /r route to do a tracked redirect', (t) => {
+  setup.server.route({
+    path: '/detour',
+    method: 'GET',
+    handler: (req, res) => {
+      res.reply({ success: true });
+    }
+  });
+  setup.server.inject({
+    url: '/r/?to=/detour&type=thatType',
+    method: 'GET',
+  }, (response) => {
+    t.equal(response.statusCode, 302);
+    setup.server.db.tracks.findOne({ type: 'thatType' }, (err, track) => {
+      t.equal(err, null);
+      t.equal(track.type, 'thatType');
+      t.equal(track.data.ip, '127.0.0.1');
+      t.equal(track.data.userAgent, 'shot');
+      t.end();
     });
   });
 });
