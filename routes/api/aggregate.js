ee4e02dc47defe14b6754f618cd3e3990b091c4f
Greg Allen <greg@firstandthird.com>
2017-04-19 15:15:33 -0700

    aggregate as separate api endpoint

---
+++ routes/api/aggregate.js
@@ -0,0 +1,29 @@
+const _ = require('lodash');
+exports.aggregate = {
+  path: '/api/report/aggregate',
+  method: 'get',
+  handler: {
+    autoInject: {
+      results(server, request, done) {
+        server.req.get('/api/report', request.query, done);
+      },
+      aggregate(server, request, results, done) {
+        const period = request.query.aggregate;
+        const dataset = results.results;
+        let grouped = _.groupBy(dataset, (item) => server.methods.processPeriod(item.createdOn, period));
+        grouped = _.mapValues(grouped, (items) => _.sumBy(items, 'value'));
+        const out = [];
+        _.forIn(grouped, (value, date) => {
+          out.push({
+            createdOn: date,
+            value
+          });
+        });
+        done(null, out);
+      },
+      reply(aggregate, done) {
+        done(null, aggregate);
+      }
+    }
+  }
+};
