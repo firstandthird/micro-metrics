150ef9962f183dd95e894bcefc43841e33465fab
chris <orthagonal1@gmail.com>
2017-04-21 12:04:00 -0500

    autoInject for method.track

--- methods/track.js
+++ methods/track.js
@@ -1,63 +1,59 @@
 /* eslint-disable strict, no-console */
 'use strict';
-
 const Joi = require('joi');
 const _ = require('lodash');
+const async = require('async');
 
 module.exports = {
-  method(payload, done) {
-    if (!done) {
-      done = _.noop;
+  method(payload, allDone) {
+    if (!allDone) {
+      allDone = _.noop;
     }
-
     const server = this;
-
-    payload.tags = server.methods.stringToKeyValue(payload.tags);
-    payload.fields = server.methods.stringToKeyValue(payload.fields);
-
-    const validation = Joi.object().keys({
-      type: Joi.string().required(),
-      tags: Joi.object(),
-      fields: Joi.object(),
-      value: Joi.any().default(1),
-      data: Joi.any(),
-      userId: Joi.string(),
-      sessionId: Joi.string()
-    });
-
-    Joi.validate(payload, validation, (err, val) => {
+    async.autoInject({
+      validate(done) {
+        const validation = Joi.object().keys({
+          type: Joi.string().required(),
+          tags: Joi.object(),
+          fields: Joi.object(),
+          value: Joi.any().default(1),
+          data: Joi.any(),
+          userId: Joi.string(),
+          sessionId: Joi.string()
+        });
+        payload.tags = server.methods.stringToKeyValue(payload.tags);
+        payload.fields = server.methods.stringToKeyValue(payload.fields);
+        Joi.validate(payload, validation, (err, data) => {
+          if (err) {
+            return done({ err, type: 'validation-error', message: 'payload failed validation' });
+          }
+          return done(null, data);
+        });
+      },
+      insert(validate, done) {
+        if (validate.tags) {
+          validate.tagKeys = Object.keys(validate.tags);
+        }
+        validate.createdOn = new Date();
+        server.db.tracks.insertOne(validate, (err, data) => {
+          if (err) {
+            return done({ err, type: 'dbErr', message: 'Error inserting data into database', data: validate });
+          }
+          return done(null, data);
+        });
+      },
+      verify(insert, done) {
+        if (insert.result.ok !== 1) {
+          return done({ err: insert.result, type: 'dbErr', message: 'Db Insert Result returned ok:0', data: insert });
+        }
+        return done(null, insert.ops[0]);
+      }
+    }, (err, results) => {
       if (err) {
-        server.log(['track', 'validation-error'], { err, message: 'payload failed validation' });
-        return done(err);
+        server.log(['track', err.type], { err: err.err, message: err.message, data: err.data });
+        return allDone(err);
       }
-      if (val.tags) {
-        val.tagKeys = Object.keys(val.tags);
-      }
-      val.createdOn = new Date();
-
-      server.db.tracks.insertOne(val, (dbErr, data) => {
-        if (dbErr) {
-          server.log(['track', 'dbError'], {
-            err,
-            message: 'Error inserting data into database',
-            payload: val
-          });
-
-          return done(dbErr);
-        }
-
-        if (data.result.ok !== 1) {
-          server.log(['track', 'dbError'], {
-            err: data.result,
-            message: 'Db Insert Result returned ok:0',
-            payload: val
-          });
-
-          return done({ err: 1, message: 'DB Insert returned ok:0' });
-        }
-
-        done(null, val);
-      });
+      return allDone(null, results.verify);
     });
   }
 };
