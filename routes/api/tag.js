37532b0464d208d6aabd8bbaada15edf0c65ac61
chris <orthagonal1@gmail.com>
2017-04-21 16:46:44 -0500

    tag is now a route

--- routes/api/tag.js
+++ routes/api/tag.js
@@ -3,17 +3,22 @@
 exports.tag = {
   method: 'GET',
   path: '/api/tag-values',
-  handler(request, reply) {
-    const filter = {};
-    filter[`tags.${request.query.tag}`] = { $exists: 1 };
-    if (request.query.type) {
-      filter.type = request.query.type;
-    }
-    request.server.methods.tag(request.query.tag, filter, (err, results) => {
-      if (err) {
-        return reply(err);
+  handler: {
+    autoInject: {
+      filter(request, server, done) {
+        const filter = {};
+        filter[`tags.${request.query.tag}`] = { $exists: 1 };
+        if (request.query.type) {
+          filter.type = request.query.type;
+        }
+        done(null, filter);
+      },
+      tag(request, server, filter, done) {
+        server.db.tracks.distinct(`tags.${request.query.tag}`, filter, done);
+      },
+      reply(tag, done) {
+        return done(null, tag);
       }
-      reply(results);
-    });
+    }
   }
 };
