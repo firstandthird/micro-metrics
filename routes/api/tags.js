e3fca160d7ce1cc44de978cc3dbe30f56c47da46
orthagonal <orthagonal1@gmail.com>
2017-04-23 20:02:31 -0500

    replace methods.tags with autoHandler and inject (#38)

--- routes/api/tags.js
+++ routes/api/tags.js
@@ -3,19 +3,24 @@
 exports.tags = {
   method: 'GET',
   path: '/api/tags',
-  handler(request, reply) {
-    const filter = {};
-    if (request.query && request.query.type) {
-      filter.type = request.query.type;
-    }
-    request.server.methods.tags(filter, (err, results) => {
-      if (err) {
-        return reply(err);
+  handler: {
+    autoInject: {
+      filter(request, server, done) {
+        const filter = {};
+        if (request.query && request.query.type) {
+          filter.type = request.query.type;
+        }
+        return done(null, filter);
+      },
+      track(request, server, filter, done) {
+        server.db.tracks.distinct('tagKeys', filter, done);
+      },
+      reply(track, done) {
+        // results will be an object in which the keys are the tag names
+        // and the values are each a list of the values for those names;
+        // eg: { 'currency': ['dollars', 'yen'], 'vegetable': 'carrot']}
+        return done(null, track);
       }
-      // results will be an object in which the keys are the tag names
-      // and the values are each a list of the values for those names
-      // eg: { 'currency': ['dollars', 'yen'], 'vegetable': 'carrot']}
-      reply(results);
-    });
+    }
   }
 };
