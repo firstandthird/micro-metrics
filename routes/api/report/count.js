3dd9a1d3d253a4b6b0281d0480b3a64fdec9ac84
chris <orthagonal1@gmail.com>
2017-04-25 17:06:47 -0500

    working report route

---
+++ routes/api/report/count.js
@@ -0,0 +1,13 @@
+'use strict';
+exports.count = {
+  path: '/api/report/count',
+  method: 'GET',
+  handler(request, reply) {
+    request.server.db.tracks.count(request.query, (err, result) => {
+      if (err) {
+        return reply(err);
+      }
+      return reply(result);
+    });
+  }
+};
