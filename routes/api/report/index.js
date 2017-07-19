391e5729c63b0f8e61418bcc6314cf7ec2e2d1c8
Eric Willis <eric@ecwillis.com>
2017-07-19 07:26:19 +0000

    Adding a group / unique to the report endpoint

--- routes/api/report/index.js
+++ routes/api/report/index.js
@@ -14,7 +14,8 @@ exports.report = {
         fields: Joi.string(),
         startDate: Joi.string(),
         endDate: Joi.string(),
-        value: Joi.number()
+        value: Joi.number(),
+        unique: Joi.string()
       }
     }
   },
@@ -25,13 +26,42 @@ exports.report = {
         const query = server.methods.getReportQuery(filter);
         done(null, query);
       },
-      find(query, server, done) {
+      find(query, request, server, done) {
+        if (request.query.unique) {
+          return done();
+        }
         server.db.tracks.find(query).sort({ createdOn: 1 }).toArray((err, results) => done(err, results));
+      },
+      findUnique(query, request, server, done) {
+        if (!request.query.unique) {
+          return done();
+        }
+
+        const uniqueId = `\$${request.query.unique}`;
+
+        // At a minimum - the unique element should exist
+        const q = Object.assign({}, query);
+        q[request.query.unique] = { $exists: 1 };
+        server.db.tracks.aggregate([
+          {
+            $match: q
+          },
+          {
+            $group: {
+              _id: uniqueId,
+              types: { $addToSet: '$type' },
+              tags: { $addToSet: '$tags' },
+              values: { $addToSet: '$value' },
+              data: { $addToSet: '$data' },
+              count: { $sum: 1 }
+            }
+          }
+        ], done);
       },
       setHeaders: (request, done) => {
         done(null, request.params.type === '.csv' ? { 'content-type': 'application/csv' } : {});
       },
-      reply(request, server, find, setHeaders, done) {
+      reply(request, server, find, findUnique, setHeaders, done) {
         if (request.params.type === '.csv') {
           find.forEach((record) => {
             delete record._id; // eslint-disable-line no-underscore-dangle
@@ -45,9 +75,11 @@ exports.report = {
           });
           return done(null, server.methods.csv(find));
         }
+
+        const fnd = find || findUnique;
         done(null, {
-          count: find.length,
-          results: find
+          count: fnd.length,
+          results: fnd,
         });
       }
     }
