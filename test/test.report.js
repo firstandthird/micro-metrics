f3b494b1fab8f6fa81bf5debd92df5498f880dc0
orthagonal <orthagonal1@gmail.com>
2017-08-06 21:35:22 -0500

    add sum to html (#56)
    * add sum to html
    
    * move column

--- test/test.report.js
+++ test/test.report.js
@@ -807,7 +807,7 @@ tap.test('can use the report method to get an aggregate list of metrics from the
   });
 });
 
-tap.test('can use the report method to get an aggregate list of metrics from the db in csv format', (t) => {
+tap.test('can use the report method to get an aggregate list of metrics from the db in html format', (t) => {
   async.autoInject({
     init(done) {
       setup.withRapptor({}, [{
@@ -882,7 +882,7 @@ tap.test('can use the report method to get an aggregate list of metrics from the
       }, (response) => {
         t.equal(response.statusCode, 200, 'returns HTTP 200');
         t.equal(typeof response.result, 'string');
-        t.notEqual(response.result.split('<tr>')[1].indexOf('<th>Date</th> <th>Avg</th> <th>Max</th> <th>Min</th> </tr>'), -1);
+        t.notEqual(response.result.split('<tr>')[1].indexOf('<th>Date</th> <th>Sum</th> <th>Avg</th> <th>Max</th> <th>Min</th> </tr>'), -1);
         t.equal(response.headers['content-type'], 'application/html');
         t.end();
       });
