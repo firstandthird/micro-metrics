b5ec74a89fa80de64bd32a9ab1111bed40244f00
Antonio Laguna <sombragriselros@gmail.com>
2017-10-19 20:32:06 +0200

    Merging data, taking precedence the IP

--- methods/extractInfo.js
+++ methods/extractInfo.js
@@ -3,10 +3,16 @@
 module.exports = {
   method(request) {
     const ipAddress = (request.headers['x-real-ip']) ? request.headers['x-real-ip'] : request.info.remoteAddress;
-    return {
+    let result = {
       ip: ipAddress,
       userAgent: request.headers['user-agent'],
       referrer: request.info.referrer
     };
+
+    if (typeof request.query.data !== 'undefined') {
+      result = Object.assign({}, this.methods.stringToKeyValue(request.query.data), result);
+    }
+
+    return result;
   }
 };
