ef83cc37338201557080cc475acccce1ad20b90d
Chris <orthagonal1@gmail.com>
2017-10-24 14:22:23 -0500

    split files, report value

--- routes/api/report/aggregate.js
+++ routes/api/report/aggregate.js
@@ -1,52 +1,6 @@
 'use strict';
 const Joi = require('joi');
 
-exports.aggregatecsv = {
-  path: 'aggregate.csv',
-  method: 'get',
-  handler: {
-    autoInject: {
-      aggregate(server, request, done) {
-        server.req.get('/api/report/aggregate', { query: request.query }, done);
-      },
-      map(aggregate, done) {
-        aggregate.forEach((record) => {
-          delete record.date;
-        });
-        done(null, aggregate);
-      },
-      csv(server, aggregate, done) {
-        return done(null, server.methods.csv(aggregate, [
-          {
-            label: 'Date',
-            value: 'dateString'
-          },
-          {
-            label: 'Sum',
-            value: 'sum'
-          },
-          {
-            label: 'Avg',
-            value: 'avg'
-          },
-          {
-            label: 'Max',
-            value: 'max'
-          },
-          {
-            label: 'Min',
-            value: 'min'
-          },
-        ]));
-      },
-      send(reply, csv, done) {
-        reply(null, csv).header('content-type', 'application/csv');
-        return done();
-      }
-    }
-  }
-};
-
 exports.aggregate = {
   path: 'aggregate',
   method: 'get',
