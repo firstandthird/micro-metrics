7d2a17b4e6fd2de0ced7155792898d3788cbbdfe
orthagonal <orthagonal1@gmail.com>
2017-10-26 14:35:08 -0500

    All conversions (#74)
    * get all conversions + test
    
    * add negative case
    
    * match only ones that start with 'conversion'

--- routes/api/conversion.js
+++ routes/api/conversion.js
@@ -1,4 +1,18 @@
 const Joi = require('joi');
+
+exports.conversions = {
+  method: 'GET',
+  path: '/api/conversions',
+  handler(request, reply) {
+    request.server.db.tracks.distinct('type', { type: { $regex: /^conversion/ } }, (err, results) => {
+      if (err) {
+        return reply(err);
+      }
+      return reply(null, results);
+    });
+  }
+};
+
 exports.conversion = {
   method: 'POST',
   path: '/api/conversion',
--- test/test.conversion.js
+++ test/test.conversion.js
@@ -310,3 +310,66 @@ tap.test('can use /c.gif route to get a conversion tracking pixel', (t) => {
     });
   });
 });
+
+tap.test('can use /api/conversions to get all conversion types', (t) => {
+  const server = setup.server;
+  async.autoInject({
+    add1(done) {
+      setup.server.req.post('/api/conversion', {
+        payload: {
+          name: 'test',
+          event: 'impression',
+          option: 'a',
+          session: '123'
+        }
+      }, done);
+    },
+    add2(done) {
+      setup.server.req.post('/api/conversion', {
+        payload: {
+          name: 'test2',
+          event: 'impression',
+          option: 'b',
+          session: '123'
+        }
+      }, done);
+    },
+    add3(done) {
+      setup.server.req.post('/api/conversion', {
+        payload: {
+          name: 'test',
+          event: 'success',
+          option: 'a',
+          session: '123'
+        }
+      }, done);
+    },
+    add4(done) {
+      setup.server.req.post('/api/track', {
+        payload: {
+          type: 'aType'
+        }
+      }, done);
+    },
+    add5(done) {
+      setup.server.req.post('/api/track', {
+        payload: {
+          type: 'not-a-conversion'
+        }
+      }, done);
+    },
+    get(add1, add2, add3, add4, add5, done) {
+      server.req.get('/api/conversions', {}, done);
+    },
+    results1(get, done) {
+      // gets a unique list of conversion tracks:
+      t.equal(get.length, 2);
+      t.notEqual(get.indexOf('conversion.test'), -1);
+      t.notEqual(get.indexOf('conversion.test2'), -1);
+      done();
+    }
+  }, (err, results) => {
+    t.equal(err, null);
+    t.end();
+  });
+});
